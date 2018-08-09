3d080666f8a3b3575c2c294efb811e96e09bd479
hingyi khong and Nikki Wong <nikki@>
2018-08-09 16:41:13 -0700

    modify if statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     var adjacentInput = parseInt($("input#adjacent").val());
     var hypotenuseInput = parseInt($("#hypotenuse").val());
 
+    console.log(oppositeInput);
+    console.log(adjacentInput);
+    console.log(hypotenuseInput);
+
     var realTriangle = function(oppositeInput, adjacentInput) {
       return oppositeInput + adjacentInput;
     };
@@ -12,17 +16,21 @@ $(document).ready(function() {
 
     if (realTriangle !== hypotenuseInput) {
       $("#non-triangle").show();
-    } else if (oppositeInput === adjacentInput && adjacentInput === hypotenuseInput) {
-      $("#equilateral").show();
     } else if (oppositeInput === adjacentInput && adjacentInput !== hypotenuseInput) {
       $("#isosceles").show();
     } else if (oppositeInput !== adjacentInput && adjacentInput === hypotenuseInput) {
       $("#isosceles").show();
     } else if (oppositeInput === hypotenuseInput && adjacentInput !== hypotenuseInput) {
       $("#isosceles").show();
-    } else {
+    } else if (oppositeInput !== hypotenuseInput && adjacentInput !== oppositeInput && adjacentInput !== hypotenuseInput) {
       $("#scalene").show();
+    } else {
+      $("#equilateral").show();
+    // } else if (oppositeInput === adjacentInput && adjacentInput === hypotenuseInput) {
+    //   $("#equilateral").show();
     }
+
+    console.log(realTriangle !== hypotenuseInput);
 
 
 
